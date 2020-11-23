463bd2f26512d6be9dbc53f793bfec00f5aa501e
MishaCherdak <cherdak.2000@mail.ru>
2020-11-23 15:27:38 +0700

    Добавил БД студентов. Настроил кнопку добавления студентов в БД. Настроил отображение БД в главном окне (при запуске и при добавлении).

--- lab5.py
+++ lab5.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+import sqlite3
 
 
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
         self.init_main()  # toolbar
+        self.db = db
+        self.view_records()  # Отображение данных
 
     def init_main(self):
         toolbar = tk.Frame(bg='#d7d8e0', bd=2)
@@ -15,17 +18,18 @@ class Main(tk.Frame):
                                     compound=tk.TOP)
         btn_open_dialog.pack(side=tk.LEFT)  # Кнопка добавить
 
-        # Отображение на экране
-        self.tree = ttk.Treeview(self, columns=('ID', "name", "surname", "patronymic", "faculty", "group",
+        self.tree = ttk.Treeview(self, columns=('ID', "name", "surname", "patronymic", "faculty", "gruppa",
                                                 "course", "direction_of_work"), height=18, show='headings')
-        self.tree.column("ID", width=30, anchor=tk.CENTER)
-        self.tree.column("name", width=70, anchor=tk.CENTER)
-        self.tree.column("surname", width=70, anchor=tk.CENTER)
-        self.tree.column("patronymic", width=70, anchor=tk.CENTER)
-        self.tree.column("faculty", width=70, anchor=tk.CENTER)
-        self.tree.column("group", width=50, anchor=tk.CENTER)
-        self.tree.column("course", width=40, anchor=tk.CENTER)
-        self.tree.column("direction_of_work", width=85, anchor=tk.CENTER)
+
+        # Отображение на экране
+        self.tree.column("ID", width=40, anchor=tk.CENTER)
+        self.tree.column("name", width=80, anchor=tk.CENTER)
+        self.tree.column("surname", width=80, anchor=tk.CENTER)
+        self.tree.column("patronymic", width=80, anchor=tk.CENTER)
+        self.tree.column("faculty", width=180, anchor=tk.CENTER)
+        self.tree.column("gruppa", width=60, anchor=tk.CENTER)
+        self.tree.column("course", width=60, anchor=tk.CENTER)
+        self.tree.column("direction_of_work", width=200, anchor=tk.CENTER)
 
         # Подписи колонок
         self.tree.heading("ID", text='ID')
@@ -33,11 +37,20 @@ class Main(tk.Frame):
         self.tree.heading("surname", text="Фамилия")
         self.tree.heading("patronymic", text="Отчество")
         self.tree.heading("faculty", text="Факультет")
-        self.tree.heading("group", text="Группа")
+        self.tree.heading("gruppa", text="Группа")
         self.tree.heading("course", text="Курс")
         self.tree.heading("direction_of_work", text="Направление работы")
 
         self.tree.pack()
+
+    def records(self, name, surname, patronymic, faculty, gruppa, course, direction_of_work):  # Передача данных в INSERT INTO
+        self.db.insert_data(name, surname, patronymic, faculty, gruppa, course, direction_of_work)
+        self.view_records()
+
+    def view_records(self):  # Отображение бд на экране
+        self.db.c.execute('''SELECT * FROM students''')
+        [self.tree.delete(i) for i in self.tree.get_children()]
+        [self.tree.insert('', 'end', values=row) for row in self.db.c.fetchall()]
 
     def open_dialog(self):
         Child()
@@ -47,6 +60,7 @@ class Child(tk.Toplevel):  # Создание окна добавить
     def __init__(self):
         super().__init__(root)
         self.init_child()
+        self.view = app
 
     def init_child(self):  # Параметры окна добавить
         self.title('Добавить студента')
@@ -62,8 +76,8 @@ class Child(tk.Toplevel):  # Создание окна добавить
         lable_patronymic.place(x=50, y=90)
         lable_select_fac = ttk.Label(self, text="Факультет:")
         lable_select_fac.place(x=50, y=120)
-        lable_group = ttk.Label(self, text="Группа:")
-        lable_group.place(x=50, y=150)
+        lable_gruppa = ttk.Label(self, text="Группа:")
+        lable_gruppa.place(x=50, y=150)
         lable_course = ttk.Label(self, text="Курс:")
         lable_course.place(x=50, y=180)
         lable_direction_of_work = ttk.Label(self, text="Нап. работы:")
@@ -79,48 +93,72 @@ class Child(tk.Toplevel):  # Создание окна добавить
         self.entry_patronymic = ttk.Entry(self)
         self.entry_patronymic.place(x=150, y=90)
 
-        self.combobox = ttk.Combobox(self, values=[u"Биологии и биотехники (ИББ)",
-                                                   u"Институт географии (ИНГЕО)",
-                                                   u"Колледж АГУ (СПО)",
-                                                   u"Исторический (ИИМО)",
-                                                   u"Институт психологии (ИП)",
-                                                   u"Искусств и дизайна (ИИД)",
-                                                   u"Институт социальных наук (ИСН)",
-                                                   u"Исторический (ИИМО)"])
-        self.combobox.current(0)
-        self.combobox.place(x=135, y=120)
+        self.combobox_faculty = ttk.Combobox(self, values=[u"Биологии и биотехники (ИББ)",
+                                                           u"Институт географии (ИНГЕО)",
+                                                           u"Колледж АГУ (СПО)",
+                                                           u"Исторический (ИИМО)",
+                                                           u"Институт психологии (ИП)",
+                                                           u"Искусств и дизайна (ИИД)",
+                                                           u"Институт социальных наук (ИСН)",
+                                                           u"Исторический (ИИМО)"])
+        self.combobox_faculty.current(0)
+        self.combobox_faculty.place(x=135, y=120)
 
-        self.entry_group = ttk.Entry(self)
-        self.entry_group.place(x=150, y=150)
+        self.entry_gruppa = ttk.Entry(self)
+        self.entry_gruppa.place(x=150, y=150)
 
-        self.combobox = ttk.Combobox(self, values=[u"Первый", u"Второй", u"Третий", u"Четвертый", u"Пятый"])
-        self.combobox.current(0)
-        self.combobox.place(x=135, y=180)
+        self.combobox_course = ttk.Combobox(self, values=[u"Первый", u"Второй", u"Третий", u"Четвертый", u"Пятый"])
+        self.combobox_course.current(0)
+        self.combobox_course.place(x=135, y=180)
 
-        self.combobox = ttk.Combobox(self, values=[u"Научное студенческое общество",
-                                                   u"Социальная поддержка",
-                                                   u"Спорт",
-                                                   u"Культура и творчество",
-                                                   u"Штаб трудовых дел"])
-        self.combobox.current(0)
-        self.combobox.place(x=135, y=210)
+        self.combobox_direction_of_work = ttk.Combobox(self, values=[u"Научное студенческое общество",
+                                                                     u"Социальная поддержка",
+                                                                     u"Спорт",
+                                                                     u"Культура и творчество",
+                                                                     u"Штаб трудовых дел"])
+        self.combobox_direction_of_work.current(0)
+        self.combobox_direction_of_work.place(x=135, y=210)
 
         btn_cansel = ttk.Button(self, text="Закрыть", command=self.destroy)
         btn_cansel.place(x=220, y=260)  # Кнопка закрыть
 
         btn_ok = ttk.Button(self, text="Добавить")  # Кнопка добавить
         btn_ok.place(x=140, y=260)
-        btn_ok.bind('<button-1>')
+        btn_ok.bind('<Button-1>', lambda event: self.view.records(self.entry_name.get(),
+                                                                  self.entry_surname.get(),
+                                                                  self.entry_patronymic.get(),
+                                                                  self.combobox_faculty.get(),
+                                                                  self.entry_gruppa.get(),
+                                                                  self.combobox_course.get(),
+                                                                  self.combobox_direction_of_work.get()))
 
         self.grab_set()
         self.focus_set()
 
 
+class DB:  # База данных
+    def __init__(self):
+        self.conn = sqlite3.connect("students.db")  # Создаем/подключаемся
+        self.c = self.conn.cursor()
+        self.c.execute(
+            '''CREATE TABLE IF NOT EXISTS students 
+            (id integer primary key, name text, surname text, 
+            patronymic text, faculty text, gruppa integer, 
+            course text, direction_of_work text)''')
+        self.conn.commit()
+
+    def insert_data(self, name, surname, patronymic, faculty, gruppa, course, direction_of_work):  # Добавление данных
+        self.c.execute('''INSERT INTO students(name, surname, patronymic, faculty, gruppa, course, direction_of_work) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)''', (name, surname, patronymic, faculty, gruppa, course, direction_of_work))
+        self.conn.commit()
+
+
 if __name__ == "__main__":  # Создание окна
     root = tk.Tk()
+    db = DB()
     app = Main(root)
     app.pack()
-    root.title("Домашние финансы")
-    root.geometry("650x450+300+200")
+    root.title("Лига студентов")
+    root.geometry("800x450+300+200")
     root.resizable(False, False)
     root.mainloop()
